19b266a6bbfdeb1bd547a12daa1ff1c9f9f61616
Tiago <tiago.farias.poa@gmail.com>
2019-08-29 19:32:18 -0300

    Add configurando apollo server

--- schema-query/projeto/index.js
+++ schema-query/projeto/index.js
@@ -1,1 +1,14 @@
-console.log('Ok, executando')
+const { ApolloServer, gql } = require ('apollo-server')
+
+const typeDefs = gql``
+
+const resolvers = { }
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listem().then(({ url }) => {
+    console.log(`Executando em ${url}`)
+})
