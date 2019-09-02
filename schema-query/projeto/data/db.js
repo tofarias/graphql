983b26c8813925356587938d15267caff827cfce
Tiago <tiago.farias.poa@gmail.com>
2019-09-01 22:22:00 -0300

    Add enum

--- schema-query/projeto/data/db.js
+++ schema-query/projeto/data/db.js
@@ -3,19 +3,22 @@ const usuarios = [{
     nome: 'John Doe',
     email: 'jd@email.com',
     idade: 31,
-    perfil_id: 1
+    perfil_id: 1,
+    status: 'ATIVO'
 },{
     id: 2,
     nome: 'Jane Doe',
     email: 'jnd@email.com',
     idade: 38,
-    perfil_id: 2
+    perfil_id: 2,
+    status: 'INATIVO'
 },{
     id: 3,
     nome: 'Martin Doe',
     email: 'md@email.com',
     idade: 26,
-    perfil_id: 1
+    perfil_id: 1,
+    status: 'BLOQUEADO'
 }]
 
 const perfis = [
