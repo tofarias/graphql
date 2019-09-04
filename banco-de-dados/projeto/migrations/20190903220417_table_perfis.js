da1f410999164156d257d26e343f15bcd542a3a1
Tiago <tiago.farias.poa@gmail.com>
2019-09-03 22:18:51 -0300

    Add insert perfis

--- banco-de-dados/projeto/migrations/20190903220417_table_perfis.js
+++ banco-de-dados/projeto/migrations/20190903220417_table_perfis.js
@@ -4,6 +4,12 @@ exports.up = function(knex) {
       table.increments('id').primary()
       table.string('nome').notNull().unique()
       table.string('rotulo').notNull()
+  }).then(function(){
+      return knex('perfis').insert([
+          {nome: 'comum', rotulo: 'Comum'},
+          {nome: 'admin', rotulo: 'Adminstrador'},
+          {nome: 'master', rotulo: 'Master'},
+      ])
   })
 };
 
