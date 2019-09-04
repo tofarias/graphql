51addc6dafdbe8ad7d92a0c669893e5a854c6de4
Tiago <tiago.farias.poa@gmail.com>
2019-09-03 22:53:24 -0300

    Add inserindo dados

---
+++ banco-de-dados/projeto/testes/insert.js
@@ -0,0 +1,24 @@
+const db = require('../config/db')
+
+/*
+const novoPerfil = {
+    nome: 'visitante',
+    rotulo: 'Visitante'
+}
+
+db('perfis').insert( novoPerfil ).then(
+    res => console.log(res)
+)
+.catch( err => console.log( err.sqlMessage ) )
+.finally(() => db.destroy() )
+*/
+
+const perfilSU = {
+    nome: 'root' + Math.random(),
+    rotulo: 'Super Usuario'
+}
+
+db.insert(perfilSU).into('perfis')
+.then(res => console.log(res))
+.catch( err => console.log( err.sqlMessage ) )
+.finally(() => db.destroy() )
